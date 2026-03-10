59050c363928c109bd8866b067746ccd952ece9d
ahmad dwi rekno <adwireknopangestu@gmail.com>
2026-03-10 12:14:37 +0700

    first commit

---
+++ 2555201001/src/pertemuan2/tipedata.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class tipedata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama="ahmaddwi";
+        System.out.println("nama :"+nama);
+        int umur=19;
+        double beratbadan=+54;
+        String prodi="ilmu komputer";
+        char kelas='A';
+        boolean laki2=true;
+        System.out.println("umur :"+umur);
+        System.out.println("beratbadan :"+beratbadan);
+        System.out.println("prodi :"+prodi);
+        System.out.println("kelas :"+kelas);
+        System.out.println("laki2 :"+laki2);
+    }
+    
+}
