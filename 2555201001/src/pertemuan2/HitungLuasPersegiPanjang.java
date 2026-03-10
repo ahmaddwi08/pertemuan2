a85bd56d92de788be96cecd25730863557488f89
ahmad dwi rekno <adwireknopangestu@gmail.com>
2026-03-10 12:07:23 +0700

    first commit

---
+++ 2555201001/src/pertemuan2/HitungLuasPersegiPanjang.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class HitungLuasPersegiPanjang {
+    public static void main(String[] args) {
+        int panjang=15;
+        int lebar=5;
+        int luas=panjang*lebar;
+        System.out.println("Hitung Luas Persegi Panjang");
+        System.out.println("panjang :"+panjang);
+        System.out.println("lebar :"+lebar);
+        System.out.println("luas :"+luas);
+    }
+    
+}
